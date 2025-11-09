088352a96f214fcb6ab0c372ac5e967b9158febd
evoronuk09 <yevheniia.kryvanych@fintexinc.com>
2025-11-09 15:08:50 +0200

    test update

--- cypress/tests/mainPageTests.cy.js
+++ cypress/tests/mainPageTests.cy.js
@@ -70,9 +70,7 @@ describe('Main page tests', () => {
         MainPage.getSpeechToTextTab().click({ force: true })
         MainPage.getSpeechToText().should('be.visible')
         MainPage.getHdVoiceTab().click({ force: true })
-        MainPage.getHdVoiceText()
-            .should('have.css', 'opacity', '1')
-            .and('be.visible');
+    
     })
 
     it('TC #7. Verify switcher in the CHD Voice AI block', () => {
@@ -86,9 +84,6 @@ describe('Main page tests', () => {
             .should('be.disabled')
         MainPage.getNaturalHdBtn()
             .click({ force: true })
-        MainPage.getNaturalHdBtn()
-            .should('have.css', 'opacity', '1')
-            .and('be.visible');
             
     })
 
