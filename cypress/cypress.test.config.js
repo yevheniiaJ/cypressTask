0e54041609ad2683b905da69a32aba9faff0b4bd
evoronuk09 <yevheniia.kryvanych@fintexinc.com>
2025-11-10 22:05:44 +0200

    config file changes

---
+++ cypress/cypress.test.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    retries:2,
+    projectId: "4g6jed",
+    video: true,
+    requestTimeout: 15000,
+    screenshotOnRunFailure: true,
+    defaultCommandTimeout: 50000,
+    pageLoadTimeout: 75000,
+    baseUrl: 'https://telnyx.com/',// should test environment here
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    //supportFile: true,
+    specPattern: 'cypress/tests/**/*.cy.js'
+  },
+});
